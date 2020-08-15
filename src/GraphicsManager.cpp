aed4c93510a4a93cb9e296f0827384a402556a71
rohan <rohan@pop-os.localdomain>
2020-08-15 14:54:10 -0400

    first working setup of OpenGL

---
+++ src/GraphicsManager.cpp
@@ -0,0 +1,40 @@
+//
+// Created by rohan on 8/15/20.
+//
+
+#include "GraphicsManager.hpp"
+
+GraphicsManager::GraphicsManager(std::string title, std::function<void(double, GraphicsManager *)> render_fn): title(title), render_fn(render_fn) {}
+
+bool GraphicsManager::execute() {
+    if(!glfwInit())
+        return false;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, gl_major_version);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, gl_minor_version);
+
+    window = glfwCreateWindow(640, 480, title.c_str(), NULL, NULL);
+    if(!window)
+    {
+        glfwTerminate();
+        return false;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(clear_color.r, clear_color.g, clear_color.b, clear_color.a);
+        glLoadIdentity();
+
+        render_fn(glfwGetTime(), this);
+
+        glFlush();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return true;
+}
---
+++ src/GraphicsManager.hpp
@@ -0,0 +1,39 @@
+//
+// Created by rohan on 8/15/20.
+//
+
+#pragma once
+
+#include <functional>
+#include <string>
+
+#include <GLFW/glfw3.h>
+#include <glm/vec3.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include "Color.hpp"
+
+class GraphicsManager {
+public:
+    GraphicsManager(std::string title, std::function<void(double, GraphicsManager*)> render_fn);
+    bool execute();
+
+    void set_gl_version(int major, int minor) {
+        gl_major_version = major;
+        gl_minor_version = minor;
+    }
+    float aspect() { return ((float)width()) / ((float)height()); }
+    int width() { return 640; }
+    int height() { return 480; }
+    void set_clear_color(Color c) { clear_color = c; }
+
+private:
+    std::string title;
+    std::function<void(double, GraphicsManager*)> render_fn;
+
+    GLFWwindow *window;
+    int gl_major_version = 2;
+    int gl_minor_version = 1;
+
+    Color clear_color = Color(0.2f, 0.2f, 0.3f, 1.0f);
+};
